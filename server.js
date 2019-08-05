9b66dc7d54819e8396e83ce53df624c42985002a
nipunupekshana <nipunupekshana1@gmail.com>
2019-08-05 08:55:51 +0530

    heroku update

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ const profile = require("./Controller/Profile");
 const image = require("./Controller/image");
 
 const db = knex({
-  client: 'pg',
+  client: "pg",
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'admin',
-    database : 'smart-brain'
+    host: "postgresql-tapered-65162",
+    user: "postgres",
+    password: "admin",
+    database: "smart-brain"
   }
 });
 
@@ -34,24 +34,7 @@ app.get('/', (req, res)=> {
 
 
 
-// app.post('/signin', (req, res) => {
-//   db.select('email', 'hash').from('login')
-//     .where('email', '=', req.body.email)
-//     .then(data => {
-//       const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-//       if (isValid) {
-//         return db.select('*').from('users')
-//           .where('email', '=', req.body.email)
-//           .then(user => {
-//             res.json(user[0])
-//           })
-//           .catch(err => res.status(400).json('unable to get user'))
-//       } else {
-//         res.status(400).json('wrong credentials')
-//       }
-//     })
-//     .catch(err => res.status(400).json('wrong credentials'))
-// });
+
 
 app.post("/signin",(req,res) => {signin.handleSignin(req,res,bcrypt,db)});
 
@@ -63,61 +46,9 @@ app.post('/register',(req,res) => {register.signinhandler(req,res,db,bcrypt)})
 
 
 
-
-// app.post('/register', (req, res) => {
-//   const { email, name, password } = req.body;
-//   const hash = bcrypt.hashSync(password);
-//     db.transaction(trx => {
-//       trx.insert({
-//         hash: hash,
-//         email: email
-//       })
-//       .into('login')
-//       .returning('email')
-//       .then(loginEmail => {
-//         return trx('users')
-//           .returning('*')
-//           .insert({
-//             email: loginEmail[0],
-//             name: name,
-//             joined: new Date()
-//           })
-//           .then(user => {
-//             res.json(user[0]);
-//           })
-//       })
-//       .then(trx.commit)
-//       .catch(trx.rollback)
-//     })
-//     .catch(err => res.status(400).json('unable to register'))
-// })
-
-// app.get('/profile/:id', (req, res) => {
-//   const { id } = req.params;
-//   db.select('*').from('users').where({id})
-//     .then(user => {
-//       if (user.length) {
-//         res.json(user[0])
-//       } else {
-//         res.status(400).json('Not found')
-//       }
-//     })
-//     .catch(err => res.status(400).json('error getting user'))
-// })
-
-
 app.get('/profile/:id',(req,res)=> {profile.profilehandler(req,res,db)});
 
-// app.put('/image', (req, res) => {
-//   const { id } = req.body;
-//   db('users').where('id', '=', id)
-//   .increment('entries', 1)
-//   .returning('entries')
-//   .then(entries => {
-//     res.json(entries[0]);
-//   })
-//   .catch(err => res.status(400).json('unable to get entries'))
-// })
+
 
 app.put('/image',(req,res) => {image.imagehandler(req,res,db)});
 
